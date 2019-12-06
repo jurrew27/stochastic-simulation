71922c41721af3ca5a21a2c2cc0ae71dd7c340bf
Jurre Wolsink <jurre.wolsink@gmail.com>
2019-12-06 18:57:50 +0100

    Add simulated annealing implementation

---
+++ simulated_annealing/main.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import permutations
+
+
+class SimulatedAnnealing:
+    def __init__(self, cities, chain_length=5, trials=5, control_start=5, cooling_method='linear', cooling_param=0.75):
+        self.cities = cities
+        self.control_start = control_start
+        self.chain_length = chain_length
+        self.trials = trials
+        self.cooling_param = cooling_param
+
+        if cooling_method == 'continuous':
+            self.calculate_new_control = self.continuous_cooling
+        elif cooling_method == 'stationary':
+            self.calculate_new_control = self.stationary_cooling
+        else:
+            self.calculate_new_control = self.linear_cooling
+
+        self.all_tours = []
+        self.all_costs = []
+        self.all_controls = []
+
+    def ratio_accepted(self):
+        # Needs to check (n!)^2 pairs, quite impossible, need to think of something smarter
+        pass
+
+    def linear_cooling(self):
+        if len(self.all_tours) % self.chain_length == 0:
+            return self.all_controls[-1] * self.cooling_param
+        else:
+            return self.all_controls[-1]
+
+    def continuous_cooling(self):
+        return self.all_controls[-1] * self.cooling_param
+
+    def stationary_cooling(self):
+        if len(self.all_tours) % self.chain_length == 0:
+            std = np.std(self.all_costs[-self.chain_length:])
+            return self.all_controls[-1] * (1 + (np.log(1 + self.cooling_param) * self.all_controls[-1] / 3 * std))**-1
+        else:
+            return self.all_controls[-1]
+
+    def length_tour(self, tour):
+        route = self.cities[tour]
+        shifted_route = np.concatenate((route[1:], route[:1]))
+        return np.sum(np.sqrt(np.sum(np.square(route - shifted_route), axis=1)))
+
+    def mutate_tour(self, tour):
+        tour = tour.copy()
+        # TODO these are often the same
+        a, b = np.random.randint(1, len(tour), 2)
+        lower = min(a, b)
+        upper = max(a, b) + 1
+        tour[lower:upper] = tour[lower:upper][::-1]
+        return tour
+
+    def do_markov_step(self):
+        tour = self.all_tours[-1]
+        cost = self.all_costs[-1]
+        new_tour = self.mutate_tour(tour)
+        new_cost = self.length_tour(new_tour)
+
+        probability_accept = min(1, np.exp(-(new_cost-cost) / self.all_controls[-1]))
+        if np.random.uniform() <= probability_accept:
+            tour = new_tour
+            cost = new_cost
+
+        return tour, cost
+
+    def run(self):
+        tour = np.concatenate(([0], np.random.permutation(range(1, len(self.cities)))))
+
+        self.all_tours = [tour]
+        self.all_costs = [self.length_tour(tour)]
+        self.all_controls = [self.control_start]
+
+        for step in range(self.trials * self.chain_length - 1):
+            tour, cost = self.do_markov_step()
+            control = self.calculate_new_control()
+
+            self.all_tours.append(tour)
+            self.all_costs.append(cost)
+            self.all_controls.append(control)
+
+        return self.all_tours, self.all_costs, self.all_controls
+
+
+def import_configuration(tsp_filename):
+    with open(tsp_filename) as tsp_file:
+        for _ in range(3):
+            next(tsp_file)
+
+        dimensions = int(tsp_file.readline().split()[2])
+        cities = np.zeros((dimensions, 2))
+
+        for _ in range(2):
+            next(tsp_file)
+
+        for i, line in enumerate(tsp_file):
+            if line.strip() == 'EOF':
+                break
+            cities[i] = line.split()[1:]
+    return cities
+
+
+if __name__ == '__main__':
+    cities = import_configuration('TSP-Configurations/test5.tsp.txt')
+    sa = SimulatedAnnealing(cities)
+    tours, costs, controls = sa.run()
+    print(tours)
+    print(costs)
+    print(controls)
